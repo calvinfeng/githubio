22491a853e411e462eef4deeee3fffd602d6ae60
Calvin Feng <cfeng@fetchrobotics.com>
2018-09-23 16:59:39 -0700

    Use video as a background for the front page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { AppBar, Typography, Toolbar, Menu, MenuItem, IconButton, Paper } from '@material-ui/core'
 import { MenuRounded } from '@material-ui/icons'
+import ReactPlayer from 'react-player'
 import GitHub from "./icons/github"
 import Post from "./components/post"
 import "./index.scss"
@@ -47,30 +48,40 @@ class Index extends React.Component<IndexProps, IndexState> {
           </IconButton>
         </Toolbar>
       </AppBar>
-
     )
   }
 
   get introduction() {
+    // <div className="logos">
+    // <img alt="Golang" src="/logos/golang.png" />
+    // <img alt="Python" src="/logos/python.png" />
+    // <img alt="JavaScript" src="/logos/javascript.png" />
+    // <img alt="Apache Kafka" src="/logos/kafka.png" />
+    // <img alt="PostgreSQL" src="/logos/postgresql.png" />
+    // <img alt="Cassandra" src="/logos/cassandra.png" />
+    // <img alt="Machine Learning" src="/logos/machine_learning.png" />
+    // <img alt="ROS" src="/logos/ros.png" />
+    // </div>
     return (
       <Paper className="introduction" elevation={1}>
-        <div className="left-container">
-          <img src="/imgs/my_face.jpeg" width="300px"/>
+        <div className="background-video">
+          <ReactPlayer
+            url={"https://www.youtube.com/watch?v=QbEWh7RNcNY"}
+            width={"1024px"}
+            height={"576px"}
+            loop={true}
+            muted={true}
+            playing={true} />
         </div>
-        <div className="right-container">
-          <p>
-            I am a Software Engineer at Fetch Robotics. From day to day, I am dealing with the 
-            following technologies.
-          </p>
-          <div className="icons">        
-          </div>
+        <div className="foreground-text">
         </div>
       </Paper>
     )
   }
 
   get content() {
-    const URLs: string[] = ["posts/lorem_ipsum.md"]
+    // const URLs: string[] = ["posts/lorem_ipsum.md"]
+    const URLs: string[] = []
     const posts = URLs.map((url) => {
       return <Post markdownURL={url} />
     })
