71354691e79f8827d0d1902d4a3be0a8c8f6889d
Calvin Feng <calvinjhfeng@gmail.com>
2018-09-24 23:55:41 -0700

    Remove extra indent spaces

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -4,36 +4,36 @@ import { Paper } from '@material-ui/core'
 import axios from 'axios';
 
 interface PostState {
-    markdownTxt: string
+  markdownTxt: string
 }
 
 interface PostProps {
-    markdownURL: string
+  markdownURL: string
 }
 
 import "./post.scss"
 class Post extends React.Component<PostProps, PostState> {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            markdownTxt: ""
-        }
+    this.state = {
+      markdownTxt: ""
     }
+  }
 
-    componentDidMount() {
-        axios.get(this.props.markdownURL).then((res) => {
-            this.setState({ markdownTxt: res.data });
-        })
-    }
+  componentDidMount() {
+    axios.get(this.props.markdownURL).then((res) => {
+      this.setState({ markdownTxt: res.data });
+    })
+  }
 
-    render() {
-        return (
-            <Paper elevation={1} className="material-paper">
-                <ReactMarkdown source={this.state.markdownTxt} />
-            </Paper>
-        )
-    }
+  render() {
+    return (
+      <Paper elevation={1} className="material-paper">
+        <ReactMarkdown source={this.state.markdownTxt} />
+      </Paper>
+    )
+  }
 }
 
 export default Post;
