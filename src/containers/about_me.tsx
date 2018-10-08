1306cbced0a36c1b1d7cddb9a8bd693cc72bf320
Calvin Feng <calvinjhfeng@gmail.com>
2018-10-07 22:49:22 -0700

    Another update to AboutMe page, index.js is re-compiled

--- src/containers/about_me.tsx
+++ src/containers/about_me.tsx
@@ -13,6 +13,7 @@ type AboutMeState = {
   background: {
     headline: string,
     subheading: string,
+    blochEqn: string,
     londonEqn1: string,
     londonEqn2: string
   }
@@ -23,12 +24,13 @@ class AboutMe extends React.Component<AboutMeProps, AboutMeState> {
     super(props)
     this.state = {
       bio: {
-        headline: "HOW I SPEND MY TIME",
-        subheading: "I ponder what to name my variables and functions with Momo"
+        headline: "WHO AM I?",
+        subheading: "I am a coding wizard and the cat next to me is my fantastic beast."
       },
       background: {
-        headline: "Background",
+        headline: "I was about to become a physicist",
         subheading: "Solid State Physics - Superconductivity",
+        blochEqn: "\\psi(\\vec{r}) = u(r)e^{i k \\cdot \\vec{r}}",
         londonEqn1: "\\frac{\\partial j_{s}}{\\partial t} = \\frac{n_{s} e^{2}}{m} E",
         londonEqn2: "\\nabla \\times j_{s} = -\\frac{n_{s}e^{2}}{m} B"
       }
@@ -39,7 +41,7 @@ class AboutMe extends React.Component<AboutMeProps, AboutMeState> {
     return (
       <section className="bio">
         <Card className="card">
-          <CardMedia image="/static/images/working.jpg" title="Boxster GTS" className="media" />
+          <CardMedia image="/static/images/working.jpg" title="Staring Screen" className="media" />
           <CardContent className="content">
             <Typography variant="headline">{this.state.bio.headline}</Typography>
             <Typography variant="subheading" color="textSecondary">{this.state.bio.subheading}</Typography>
@@ -50,6 +52,83 @@ class AboutMe extends React.Component<AboutMeProps, AboutMeState> {
           </CardContent>
         </Card>
       </section>
+    )
+  }
+
+  get activities() {
+    return (
+      <section className="activities">
+        <Grid container className="column" direction="column">
+          <Grid container className="row" direction="row" spacing={16} justify="center" >
+            {this._surfing}
+            {this._driving}
+          </Grid>
+        </Grid>
+      </section>
+    )
+  }
+
+  get _surfing() {
+    return (
+      <Grid item>
+        <Card className="medium-card">
+          <CardHeader title="I am a surfer" subheader="" />
+          <CardMedia image="/static/images/surfing.jpg" title="Surfing" className="media" />
+          <CardContent>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan tortor ante, varius blandit est
+              vestibulum in. Quisque non libero massa.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    )
+  }
+
+  get _driving() {
+    return (
+      <Grid item>
+        <Card className="medium-card">
+          <CardHeader title="I am a car enthusiast" subheader="" />
+          <CardMedia image="/static/images/rudolph.jpg" title="Boxster GTS" className="media" />
+          <CardContent>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan tortor ante, varius blandit est
+              vestibulum in. Quisque non libero massa.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    )
+  }
+
+  get _traveling() {
+    return (
+      <Grid item>
+        <Card className="long-card">
+          <CardHeader title="I am a traveler" subheader="That one time I went to the snowy mountain" />
+          <CardMedia image="/static/images/snowy.jpg" title="Snow Monkey Park" className="media" />
+        </Card>
+      </Grid>
+    )  
+  }
+
+  get _videography() {
+    return (
+      <Grid item>
+        <Card className="long-card">
+          <CardHeader title="I am a photographer" subheader="I actually really enjoy making videos" />
+          <ReactPlayer
+              url={"https://www.youtube.com/watch?v=_GiKP-ZMXvQ"}
+              width={"1024px"}
+              height={"576px"} />
+          <CardContent>
+            <Typography component="p">
+              Why do I enjoy driving? Because I get to make some pretty cool videos with a drone.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
     )
   }
 
@@ -71,9 +150,11 @@ class AboutMe extends React.Component<AboutMeProps, AboutMeState> {
       <CardContent className="video-content">
         <ReactPlayer
           url={"https://www.youtube.com/watch?time_continue=4&v=Xts42tFYRRg"}
-          height={"350px"} />
+          height={"350px"}
+          className="player" />
         <Typography variant="body1" className="eqn">
           <MathJax.Provider>
+            <MathJax.Node formula={this.state.background.blochEqn} />
             <MathJax.Node formula={this.state.background.londonEqn1} />
             <MathJax.Node formula={this.state.background.londonEqn2} />
           </MathJax.Provider>
@@ -91,78 +172,12 @@ class AboutMe extends React.Component<AboutMeProps, AboutMeState> {
     )
   }
 
-  get activities() {
-    return (
-      <section className="activities">
-        <Grid container className="column" direction="column">
-          <Grid container className="row" direction="row" spacing={16} justify="center" >
-            {this._surfing}
-            {this._driving}
-            {this._traveling}
-          </Grid>
-        </Grid>
-      </section>
-    )
-  }
-
-  get _surfing() {
-    return (
-      <Grid item>
-        <Card className="medium-card">
-          <CardHeader title="What do I used to do?" subheader="Surf with a GoPro but I'm not that pro" />
-          <CardMedia image="/static/images/surfing.jpg" title="Surfing" className="media" />
-        </Card>
-      </Grid>
-    )
-  }
-
-  get _driving() {
-    return (
-      <Grid item>
-        <Card className="medium-card">
-          <CardHeader title="What do I do besides coding?" subheader="Drive up to the mountains" />
-          <CardMedia image="/static/images/rudolph.jpg" title="Boxster GTS" className="media" />
-        </Card>
-      </Grid>
-    )
-  }
-
-  get _traveling() {
-    return (
-      <Grid item>
-        <Card className="long-card">
-          <CardHeader title="Traveling" subheader="That one time I went to the snowy mountain" />
-          <CardMedia image="/static/images/snowy.jpg" title="Snow Monkey Park" className="media" />
-        </Card>
-      </Grid>
-    )  
-  }
-
-  get _videography() {
-    return (
-      <Grid item>
-        <Card className="long-card">
-          <CardHeader title="What else?" subheader="I actually really enjoy making videos" />
-          <ReactPlayer
-              url={"https://www.youtube.com/watch?v=_GiKP-ZMXvQ"}
-              width={"1024px"}
-              height={"576px"} />
-          <CardContent>
-            <Typography component="p">
-              Why do I enjoy driving? Because I get to make some pretty cool videos with a drone.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    )
-  }
-
   render() {
     return (
       <section className="about-me">
         {this.bio}
-        {this.background}
         {this.activities}
+        {this.background}
       </section>
     )
   }
